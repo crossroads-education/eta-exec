9c0a42978e4af8dada17b165f2af864cb3f1bb5e
Alex Hicks <aldahick@iupui.edu>
2016-06-24 09:10:00 -0400

    Move Configuration to lib

--- lib/autoload.ts
+++ lib/autoload.ts
@@ -6,11 +6,6 @@ import "es6-shim"; // Stuff like String.prototype.startsWith() that isn't in ES5
 import * as eta from "eta-lib";
 import * as fs from "fs";
 
-// *** template imports ***
-import {Configuration} from "./interfaces/Configuration";
-
-// *** variable exports ***
-export let config : Configuration = JSON.parse(fs.readFileSync("./config/main.json").toString());
 export let root : string = process.cwd().replace(/\\/g, "/") + "/"; // root of this repository
 
 // *** class exports ***
