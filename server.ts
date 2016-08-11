ec07b62aa5e72f47cdf0dc0d4ed34cc0dae8a5e8
Alex Hicks <aldahick@iupui.edu>
2016-08-11 13:55:33 -0400

    Prevent uncaught errors from crashing the server

--- server.ts
+++ server.ts
@@ -1,5 +1,10 @@
 require("source-map-support").install();
+
+process.on("uncaughtException", function(err : Error) {
+    console.log("An uncaught error occurred: " + err.message);
+    console.log(err.stack);
+});
+
 import * as site from "./lib/autoload";
 
 site.server.init("modules"); // use ./modules/ as a module directory
-// site.server.start(); // start listening
